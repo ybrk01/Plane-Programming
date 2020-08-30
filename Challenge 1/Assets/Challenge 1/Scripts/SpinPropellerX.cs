2f75df06095f351ad2969dde0bc71eb34f592ba1
ybrk01 <58778553+ybrk01@users.noreply.github.com>
2020-08-31 00:52:30 +0300

    master

---
+++ Challenge 1/Assets/Challenge 1/Scripts/SpinPropellerX.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinPropellerX : MonoBehaviour
+{
+    public GameObject propeller;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        propeller.transform.Rotate(Vector3.forward * 50);
+    }
+}
